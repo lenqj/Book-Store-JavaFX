b1c8f8d19bf720c50b10cfbf4da2cf2c0991b21f
unknown <ionelrus022@yahoo.com>
2023-11-27 15:14:57 +0200

    Removed old Main class and added new Main on gradle build

--- Proiect/src/main/java/Main.java
+++ Proiect/src/main/java/Main.java
@@ -41,8 +41,7 @@ public class Main extends Application {
 
         final LoginView loginView = new LoginView(primaryStage);
 
-        final UserValidator userValidator = new UserValidator(userRepository);
 
-        new LoginController(loginView, authenticationService, userValidator);
+        new LoginController(loginView);
     }
 }
